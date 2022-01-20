7b7dcadc24f21650a100614e6eecd821e013f14a
selankon <selankon@selankon.xyz>
2022-01-20 17:07:16 +0100

    Use generics badges

--- components/blocks/process-status-label.tsx
+++ components/blocks/process-status-label.tsx
@@ -1,59 +1,33 @@
 import React from 'react'
-import styled from 'styled-components'
-import { useTranslation } from 'react-i18next'
+import { ActiveBadge, UpcomingBadge, EndedBadge, CanceledBadge } from '@components/elements/text-badge'
+// import { useTranslation } from 'react-i18next'
 import { VoteStatus } from '@lib/util'
+import i18n from '@i18n'
 
 interface IProcessStatusLabelProps {
   status : VoteStatus
 }
 
 export const ProcessStatusLabel = ({status}: IProcessStatusLabelProps) => {
-  const { i18n } = useTranslation()
+  // const { i18n } = useTranslation()
 
   switch (status) {
     case VoteStatus.Active:
-        return <ActiveProcessStatusLabel>{i18n.t('vote.active_vote')}</ActiveProcessStatusLabel>
+        return <ActiveBadge>{i18n.t('vote.active_vote')}</ActiveBadge>
 
     case VoteStatus.Upcoming:
-        return <UpcomingStatusLabel>{i18n.t('vote.upcoming_vote')}</UpcomingStatusLabel>
+        return <UpcomingBadge>{i18n.t('vote.upcoming_vote')}</UpcomingBadge>
 
     case VoteStatus.Ended:
-      return <EndedProcessStatusLabel>{i18n.t('vote.ended_vote')}</EndedProcessStatusLabel>
+      return <EndedBadge>{i18n.t('vote.ended_vote')}</EndedBadge>
 
     case VoteStatus.Paused:
-      return <EndedProcessStatusLabel>{i18n.t('vote.paused_vote')}</EndedProcessStatusLabel>
+      return <EndedBadge>{i18n.t('vote.paused_vote')}</EndedBadge>
 
     case VoteStatus.Canceled:
-      return <CanceledProcessStatusLabel>{i18n.t('vote.canceled_vote')}</CanceledProcessStatusLabel>
+      return <CanceledBadge>{i18n.t('vote.canceled_vote')}</CanceledBadge>
 
     default:
       return <></>
   }
 }
-
-const BaseProcessStatusLabel = styled.span`
-  box-shadow: rgba(180, 193, 228, 0.35) 0px 3px 3px;
-  border-radius: 10px;
-  height: 16px;
-  padding: 2px 8px;
-  font-size: 12px;
-  font-weight: 500;
-  color: ${({theme}) => theme.white}
-`
-
-const ActiveProcessStatusLabel = styled(BaseProcessStatusLabel)`
-  background-color: ${({theme}) => theme.accent1C}
-`
-
-const UpcomingStatusLabel = styled(BaseProcessStatusLabel)`
-  background-color: ${({theme}) => theme.accent1B}
-`
-
-const EndedProcessStatusLabel = styled(BaseProcessStatusLabel)`
-  background-color: ${({theme}) => theme.textAccent2}
-`
-
-const CanceledProcessStatusLabel = styled(BaseProcessStatusLabel)`
-  background-color:  ${({theme}) => theme.danger}
-`
-
