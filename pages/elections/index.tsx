3a634bf1c65745adc84531567a2a7d7d516165f7
selankon <selankon@selankon.xyz>
2022-01-21 12:25:50 +0100

    Refactor to prevent no election id on url

--- pages/elections/index.tsx
+++ pages/elections/index.tsx
@@ -1,34 +1,37 @@
-import { Loader } from "@components/blocks/loader"
-import ElectionDetailPage from "@components/pages/elections/details"
+import { Loader } from '@components/blocks/loader'
+import ElectionDetailPage from '@components/pages/elections/details'
 import { ViewContext, ViewStrategy } from '@lib/strategy'
-import { useProcess } from "@vocdoni/react-hooks"
+import { useProcess } from '@vocdoni/react-hooks'
 import { useUrlHash } from 'use-url-hash'
 
-
-
 const ElectionsPage = () => {
-
+  let strategies: ViewStrategy[] = [];
   const processId = useUrlHash().slice(1)
-  const { process: processInfo, error, loading } = useProcess(processId)
 
-  const renderEntityPage = new ViewStrategy(
-    () => !!processInfo && !loading,
-    <ElectionDetailPage 
-      processId={processId} 
-      processInfo={processInfo} />
-  )
-  const renderLoadingPage = new ViewStrategy(
-    () => true,
-    <Loader visible />
-  )
+  if (processId.length !== 0) {
+    // todo(ritmo): show an error page
+    const { process: processInfo, error, loading } = useProcess(processId)
+    const renderElectionDetailsPage = new ViewStrategy(
+      () => !!processInfo && !loading,
+      <ElectionDetailPage processId={processId} processInfo={processInfo} />
+    )
+    strategies.push(renderElectionDetailsPage)
+  } 
+  else {
+    const renderShowElectionPage = new ViewStrategy(
+      () => true,
+      <h1>Election list</h1>
+    )
+    strategies.push(renderShowElectionPage)
+  }
 
-  const viewContext = new ViewContext([
-    renderEntityPage,
-    renderLoadingPage
-  ])
+  
+  const renderLoadingPage = new ViewStrategy(() => true, <Loader visible />)
+  strategies.push(renderLoadingPage)
+
+  const viewContext = new ViewContext(strategies)
 
   return viewContext.getView()
-  // return <ElectionDetailPage></ElectionDetailPage>
 }
 
 export default ElectionsPage
