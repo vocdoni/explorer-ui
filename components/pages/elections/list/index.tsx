424948a96f6bbddfeb9c0212023e03616279d701
selankon <selankon@selankon.xyz>
2022-02-17 15:00:52 +0100

    Refactor to rue a ListPageTemplate component

--- components/pages/elections/list/index.tsx
+++ components/pages/elections/list/index.tsx
@@ -1,32 +1,27 @@
-import { Typography, TypographyVariant } from '@components/elements/typography';
+import { Typography, TypographyVariant } from '@components/elements/typography'
 import React, { useEffect, useState } from 'react'
 import { DashboardProcessList } from './process-list'
 import i18n from '@i18n'
 import { colors } from '@theme/colors'
-import { useProcessCount } from '@hooks/use-processes';
+import { useProcessCount } from '@hooks/use-processes'
+import { ListPageTemplate } from '@components/pages/app/page-templates/list-page-template'
 
-interface IDashboardShowProcessesProps {
-}
+interface IDashboardShowProcessesProps {}
 
 const PROCESS_PER_PAGE = 10
 
-export const DashboardShowProcesses = ({
-}: IDashboardShowProcessesProps) => {
-  const {processCount} = useProcessCount({});
- 
+export const DashboardShowProcesses = ({}: IDashboardShowProcessesProps) => {
+  const { processCount } = useProcessCount({})
 
   return (
-    <>
-      <Typography variant={TypographyVariant.H3} color={colors.blueText}>
-          {i18n.t('elections_list.elections_list_title')}
-        </Typography>
-        <Typography variant={TypographyVariant.Small} color={colors.lightText}>
-          {i18n.t('elections_list.total_n_processes')} {processCount}
-        </Typography>
-
-      <DashboardProcessList 
-        totalProcessCount={processCount} 
-        pageSize={PROCESS_PER_PAGE}/>
-    </>
+    <ListPageTemplate
+      title={i18n.t('elections_list.elections_list_title')}
+      subtitle={i18n.t('elections_list.total_n_processes') + processCount}
+    >
+      <DashboardProcessList
+        totalProcessCount={processCount}
+        pageSize={PROCESS_PER_PAGE}
+      />
+    </ListPageTemplate>
   )
 }
--- components/pages/organizations/list/index.tsx
+++ components/pages/organizations/list/index.tsx
@@ -1,29 +1,24 @@
-import { Typography, TypographyVariant } from "@components/elements/typography";
-import { useEntityCount } from "@hooks/use-entities";
-import { colors } from "@theme/colors";
+import { Typography, TypographyVariant } from '@components/elements/typography'
+import { useEntityCount } from '@hooks/use-entities'
+import { colors } from '@theme/colors'
 import i18n from '@i18n'
+import { ListPageTemplate } from '@components/pages/app/page-templates/list-page-template'
 
-
-interface IDashboardShowEntitiesProps {
-}
+interface IDashboardShowEntitiesProps {}
 const ENTITIES_PER_PAGE = 10
 
-export const DashboardShowEntities = ({
+export const DashboardShowEntities = ({}: IDashboardShowEntitiesProps) => {
+  const { entitiesCount } = useEntityCount({})
 
-} : IDashboardShowEntitiesProps)  => {
-    const {entitiesCount} = useEntityCount({});
-
-    return (<>
-      <Typography variant={TypographyVariant.H3} color={colors.blueText}>
-          {i18n.t('organizations_list.organizations_list_title')}
-        </Typography>
-        <Typography variant={TypographyVariant.Small} color={colors.lightText}>
-          {i18n.t('organizations_list.total_n_organizations')} {entitiesCount}
-        </Typography>
-
-      {/* <DashboardProcessList 
-        totalProcessCount={processCount} 
-        pageSize={PROCESS_PER_PAGE}/> */}
-    </>
+  return (
+    <ListPageTemplate
+      title={i18n.t('organizations_list.organizations_list_title')}
+      subtitle={i18n.t('organizations_list.total_n_organizations') + entitiesCount}
+    >
+      {/* <DashboardProcessList
+        totalProcessCount={processCount}
+        pageSize={PROCESS_PER_PAGE}
+      /> */}
+    </ListPageTemplate>
   )
 }
