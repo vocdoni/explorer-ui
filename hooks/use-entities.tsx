777a6b8071c09afb39954b450cfabb7fefea12cc
selankon <selankon@selankon.xyz>
2022-02-21 12:25:21 +0100

    Implement useEntityList hook

--- hooks/use-entities.tsx
+++ hooks/use-entities.tsx
@@ -4,6 +4,49 @@ import { useAlertMessage } from './message-alert'
 import i18n from '../i18n'
 import { fetchMethod } from '@lib/api'
 
+export const useEntityList = ({searchTerm, from}: {searchTerm?: string, from?: number}) => {
+  const { setAlertMessage } = useAlertMessage()
+  const { poolPromise } = usePool()
+  const [entitiesList, setEntitiesList] = useState([])
+  const [loadingEntitiesList, setLoadingEntitiesList] = useState(false)
+
+  const getEntityIDs = useCallback(() => {
+    setLoadingEntitiesList(true)
+    poolPromise
+      .then((pool) =>{
+        // todo(kon): this method is not exposed yet to dvotejs
+        return fetchMethod(pool, {
+          method: 'getEntityList',
+          params: {
+            searchTerm: searchTerm, from: from
+          },
+        })
+      })
+      .then((response) => {
+        console.debug('DEBUG', 'getEntityList', response['response'])
+        if (!response['response']['ok'])
+          throw new Error('Error retrieving getProcessCount')
+          setEntitiesList(response['response']['entityIds'])
+      })
+      .catch((err) => {
+        setLoadingEntitiesList(false)
+        console.error(err)
+        setAlertMessage(i18n.t('errors.the_list_of_entities_cannot_be_loaded'))
+      })
+
+  }, [from, poolPromise, searchTerm, setAlertMessage])
+
+  useEffect(() => {
+    getEntityIDs()
+  }, [searchTerm, from, getEntityIDs])
+
+  return {
+    entitiesList,
+    loadingEntitiesList
+  }
+
+}
+
 /** Get entity count */
 export const useEntityCount = () => {
   const { setAlertMessage } = useAlertMessage()
