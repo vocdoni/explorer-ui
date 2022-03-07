a38c4b73025aa1518efd6f57273f56477a6379cf
selankon <selankon@selankon.xyz>
2022-03-07 12:09:45 +0100

    Implement block card as generic card

--- components/blocks/card/block-card.tsx
+++ components/blocks/card/block-card.tsx
@@ -1,26 +1,55 @@
 import { Card } from '@components/elements/cards'
 import { ColumnProps } from '@components/elements/grid'
+import { BLOCKS_DETAILS } from '@const/routes'
 import i18n from '@i18n'
 import { localizedDateDiff } from '@lib/date'
+import RouterService from '@lib/router'
 import { BlockInfo } from '@lib/types'
+import { GenericListItemWithBadge } from '../list-items'
 
-export const BlockCard = ({sm, md, lg, xl, blockData, proposerShrink}: ColumnProps & { blockData: BlockInfo, proposerShrink?: number }) => {
-    const proposer = proposerShrink ? blockData.proposer_address.substring(0, proposerShrink) + "..." : blockData.proposer_address
-    return (
-    <Card sm={sm} md={md} lg={lg} xl={xl} key={blockData.height}>
-      <h4>
-        {i18n.t('blocks.number')} {blockData.height}
-      </h4>
+export const BlockCard = ({
+  sm,
+  md,
+  lg,
+  xl,
+  blockData,
+  proposerShrink,
+  moreDetails = false,
+}: ColumnProps & {
+  blockData: BlockInfo
+  proposerShrink?: number
+  moreDetails?: boolean
+}) => {
+  const proposer = proposerShrink
+    ? blockData?.proposer_address.substring(0, proposerShrink) + '...'
+    : blockData?.proposer_address
+
+  return (
+    <GenericListItemWithBadge
+      sm={sm}
+      md={md}
+      lg={lg}
+      xl={xl}
+      key={blockData?.height}
+      topLeft={
+        <>
+          {i18n.t('blocks.number')} {blockData?.height}
+        </>
+      }
+      badge={
+        <>
+          {i18n.t('blocks.transactions')} {blockData?.num_txs}
+        </>
+      }
+      dateText={localizedDateDiff(new Date(blockData?.timestamp))}
+      link={RouterService.instance.get(BLOCKS_DETAILS, {
+        blockHeight: blockData?.height.toString(),
+      })}
+      // title={}
+    >
       <p>
-        <small>{localizedDateDiff(new Date(blockData.timestamp))}</small>
+        {i18n.t('blocks.proposer')}: <code>0x{proposer}</code>
       </p>
-      <p>
-        {i18n.t('blocks.transactions')}: {blockData.num_txs}
-      </p>
-      <p>
-        {i18n.t('blocks.proposer')}:{' '}
-        <code>0x{proposer}</code>
-      </p>
-    </Card>
+    </GenericListItemWithBadge>
   )
 }
--- const/routes.ts
+++ const/routes.ts
@@ -5,6 +5,7 @@ export const ENVELOPES_PATH = "/envelopes"
 export const ELECTIONS_PATH = "/elections"
 export const ELECTIONS_DETAILS = '/elections/show/#/{electionsId}'
 export const BLOCKS_PATH = "/blocks"
+export const BLOCKS_DETAILS = "/blocks/show/#/{blockHeight}"
 export const TRANSACTIONS_PATH = "/transactions"
 export const VALIDATORS_PATH = "/validators"
 export const STATS_PATH = "/stats"
