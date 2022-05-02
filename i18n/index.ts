87a5692f2aa707a460266136014e030463ab37ee
selankon <selankon@selankon.xyz>
2022-05-02 10:45:38 +0200

    Use initReactI18next

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,5 +1,6 @@
 import i18next from 'i18next'
 import translation from './locales'
+import { initReactI18next } from "react-i18next";
 
 const i18n = i18next.createInstance()
 
@@ -7,22 +8,24 @@ export const supportedLanguages = ['ca', 'en', 'eo', 'es']
 
 const userLang = (typeof window !== 'undefined' && typeof window.navigator.language !== 'undefined') ? window.navigator.language.substr(0, 2).toLowerCase() : process.env.LANG
 
-i18n.init({
-	debug: process.env.NODE_ENV === 'development',
-	lng: userLang,
-	fallbackLng: 'en',
-	defaultNS: 'translation',
-	interpolation: {
-		escapeValue: false,
-	},
-	returnEmptyString: false,
-})
+i18n
+	.use(initReactI18next)
+	.init({
+		debug: process.env.NODE_ENV === 'development',
+		lng: userLang,
+		fallbackLng: 'en',
+		defaultNS: 'translation',
+		interpolation: {
+			escapeValue: false,
+		},
+		returnEmptyString: false,
+	})
 
 
 for (const lang of supportedLanguages) {
-  if (typeof translation[lang] !== 'undefined') {
-      i18n.addResourceBundle(lang, 'translation', translation[lang])
-  }
+	if (typeof translation[lang] !== 'undefined') {
+		i18n.addResourceBundle(lang, 'translation', translation[lang])
+	}
 }
 
 export default i18n
