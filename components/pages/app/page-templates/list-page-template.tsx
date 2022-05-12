411b13555c01711636c01314a93f9c0c7b8ce85c
selankon <selankon@selankon.xyz>
2022-05-12 17:44:26 +0200

    Implement one line title/filter for entities

--- components/pages/app/page-templates/list-page-template.tsx
+++ components/pages/app/page-templates/list-page-template.tsx
@@ -1,12 +1,12 @@
+import { FlexAlignItem, FlexContainer, FlexJustifyContent } from "@components/elements/flex"
 import { Typography, TypographyVariant } from "@components/elements/typography"
 import { colors } from "@theme/colors"
 import { ReactNode } from "react"
+import styled from "styled-components"
 
 interface IListPageTemplateProps {
     title: string
-    subtitle: string
-    children: ReactNode
-    
+    subtitle: string    
 }
 
 /**
@@ -15,18 +15,36 @@ interface IListPageTemplateProps {
  * @param 
  * @returns 
  */
-export const ListPageTemplate = ({ title, subtitle, children
+export const ListPageTitle = ({ title, subtitle
 }: IListPageTemplateProps) => { 
 
   return (
-    <>
+    <div>
       <Typography variant={TypographyVariant.H3} color={colors.blueText} margin='0px auto auto'>
           {title}
         </Typography>
         <Typography variant={TypographyVariant.Small} color={colors.lightText}>
           {subtitle}
         </Typography>
-        {children}
-    </>
+    </div>
   )
 }
+
+export const InlineTitleChildrenContainer=({title, children} : {title: ReactNode, children: ReactNode}) => {
+  return (<TopDiv>
+    <FlexContainer
+      alignItem={FlexAlignItem.Center}
+      justify={FlexJustifyContent.Center}
+    >
+      {title}
+    </FlexContainer>
+    {children}
+  </TopDiv>)
+}
+
+const TopDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 10px;
+`
--- components/pages/organizations/list/entity-list.tsx
+++ components/pages/organizations/list/entity-list.tsx
@@ -1,29 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 
-
-import { PaginatedListTemplate, usePaginatedList } from '@components/pages/app/page-templates/paginated-list-template'
+import {
+  PaginatedListTemplate,
+  usePaginatedList,
+} from '@components/pages/app/page-templates/paginated-list-template'
 import { EntitiesFilter, IFilterEntity } from '../components/entities-filter'
 import { useEntityList } from '@hooks/use-entities'
 import { DashboardEntityListItem } from './entity-list-item'
+import { InlineTitleChildrenContainer } from '@components/pages/app/page-templates/list-page-template'
 
 interface IDashboardProcessListProps {
   loading?: boolean
   pageSize?: number
   totalCount?: number
+  title: ReactNode
 }
 
 export const DashboardEntityList = ({
   pageSize = 8,
   totalCount = 0,
+  title,
 }: IDashboardProcessListProps) => {
-
   // Render item on the list from it summary
   const renderProcessItem = (identity: string) => {
     return (
       <div key={identity}>
-        <DashboardEntityListItem
-          entityId={identity}
-        />
+        <DashboardEntityListItem entityId={identity} />
       </div>
     )
   }
@@ -34,7 +36,7 @@ export const DashboardEntityList = ({
   const { entitiesList, loadingEntitiesList } = useEntityList({
     from: dataPagination,
     searchTerm: filter?.searchTerm,
-    listSize: pageSize
+    listSize: pageSize,
   })
 
   // Set loading
@@ -46,27 +48,34 @@ export const DashboardEntityList = ({
   const {
     currentPage,
     methods: { enableFilter, disableFilter, setCurrentPage },
-  } = usePaginatedList<IFilterEntity>({pageSize, filter, setFilter, setDataPagination})
+  } = usePaginatedList<IFilterEntity>({
+    pageSize,
+    filter,
+    setFilter,
+    setDataPagination,
+  })
 
   return (
     <>
-      <EntitiesFilter
-        onEnableFilter={enableFilter}
-        onDisableFilter={disableFilter}
-      ></EntitiesFilter>
+      <InlineTitleChildrenContainer title={title}>
+        <EntitiesFilter
+          onEnableFilter={enableFilter}
+          onDisableFilter={disableFilter}
+        ></EntitiesFilter>
+      </InlineTitleChildrenContainer>
+
       <PaginatedListTemplate
         loading={loading}
         elementsList={!entitiesList.length ? [] : entitiesList}
         totalElementsCount={
           // When using filters you don't know the total count. So it don't handle last page pagination
-          Object.keys(filter).length === 0
-            ? totalCount
-            : null}
+          Object.keys(filter).length === 0 ? totalCount : null
+        }
         renderElementFunction={renderProcessItem}
-        pageSize={pageSize} 
-        currentPage={currentPage} 
-        setCurrentPage={setCurrentPage}      />
+        pageSize={pageSize}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </>
   )
 }
-
