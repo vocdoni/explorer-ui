2a87a95d2f6aff02353a39f0f0151f9aa45a1700
selankon <selankon@selankon.xyz>
2022-05-12 17:53:09 +0200

    Implement one line title/filter for transactions

--- components/pages/transactions/list/index.tsx
+++ components/pages/transactions/list/index.tsx
@@ -1,8 +1,7 @@
 import { useTranslation } from 'react-i18next'
-import { ListPageTemplate } from '@components/pages/app/page-templates/list-page-template'
+import { ListPageTitle } from '@components/pages/app/page-templates/list-page-template'
 import { DashboardTransactionsList } from './transaction-list'
 import { useTransactionCount } from '@hooks/use-transactions'
-
 
 export const DashboardShowTransactions = () => {
   const { i18n } = useTranslation()
@@ -10,11 +9,17 @@ export const DashboardShowTransactions = () => {
   const page_size = 10
 
   return (
-    <ListPageTemplate
-      title={i18n.t('transactions.transactions_list_title')}
-      subtitle={i18n.t('transactions.total_n_transactions', {transactionCount: transactionCount})}
-    >
-      <DashboardTransactionsList transactionHeight={transactionCount} pageSize={page_size}></DashboardTransactionsList>
-    </ListPageTemplate>
+    <DashboardTransactionsList
+      transactionHeight={transactionCount}
+      pageSize={page_size}
+      title={
+        <ListPageTitle
+          title={i18n.t('transactions.transactions_list_title')}
+          subtitle={i18n.t('transactions.total_n_transactions', {
+            transactionCount: transactionCount,
+          })}
+        />
+      }
+    ></DashboardTransactionsList>
   )
 }
