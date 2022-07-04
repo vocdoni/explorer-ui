364d9b2164c94f7affbdf8c85485123b01413f66
selankon <selankon@selankon.xyz>
2022-07-04 11:31:58 +0200

    Fix top height margin

--- pages/stats/index.tsx
+++ pages/stats/index.tsx
@@ -6,6 +6,7 @@ import { useBlocks } from '@hooks/use-blocks'
 import { Else, If, Then } from 'react-if'
 import { useTranslation } from 'react-i18next'
 import { HeroBanner } from '@components/pages/home/components/hero-banner'
+import styled from 'styled-components'
 
 const StatsPageIndex = () => {
   const { loading: loadingStats, stats } = useStats({})
@@ -19,12 +20,14 @@ const StatsPageIndex = () => {
       <Else>
         {stats !== undefined || !loadingStats ? (
           <>
-            <HeroBanner
-              processes={stats?.process_count}
-              organizations={stats?.entity_count}
-              averageBlockTime={(stats?.block_time[0] || 0) / 1000}
-              envelopes={stats?.envelope_count}
-            />
+            <BannerContainer>
+              <HeroBanner
+                processes={stats?.process_count}
+                organizations={stats?.entity_count}
+                averageBlockTime={(stats?.block_time[0] || 0) / 1000}
+                envelopes={stats?.envelope_count}
+              />
+            </BannerContainer>
             <StatsPage stats={stats} />
           </>
         ) : (
@@ -34,5 +37,18 @@ const StatsPageIndex = () => {
     </If>
   )
 }
+
+const BannerContainer = styled.div`
+  margin-top: -110px;
+  padding-top: 40px;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+
+  @media ${({ theme }) => theme.screenMax.tablet} {
+    height: auto;
+  }
+
+`
 
 export default StatsPageIndex
