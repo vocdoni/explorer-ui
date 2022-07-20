004cf1af3d9e4f0da0bb2a460ee1079c27b8c10d
selankon <selankon@selankon.xyz>
2022-07-20 09:05:53 +0200

    Fix revelaed keys titles

--- components/pages/processes/components/process_keys.tsx
+++ components/pages/processes/components/process_keys.tsx
@@ -1,6 +1,8 @@
 import { StatusCard } from '@components/elements/cards'
 import { Grid } from '@components/elements/grid'
+import { Typography, TypographyVariant } from '@components/elements/typography'
 import { useProcessKeys } from '@hooks/use-process-keys'
+import { colors } from '@theme/colors'
 import { useTranslation } from 'react-i18next'
 
 
@@ -16,23 +18,29 @@ export const EncryptionKeys = ({
   return (
     <>
     { !loadingProcessKeys &&
-      <Grid>
-        <StatusCard
-          sm={1}
-          md={3}
-          title={i18n.t('processes.details.published_keys')}
-          rightText=""
-        >
-          <h3>{processKeys?.encryptionPubKeys?.length ?? 0}</h3>
-        </StatusCard>
-        <StatusCard
-          md={3}
-          title={i18n.t('processes.details.encrypted_keys')}
-          rightText=""
-        >
-          <h3>{processKeys?.encryptionPrivKeys?.length ?? 0}</h3>
-        </StatusCard>
-      </Grid>}
+      <>
+        <Typography variant={TypographyVariant.Small} color={colors.lightText} margin={'16px 0 0 0'}>
+          <span>{i18n.t('processes.details.election_encryption_keys')}</span>
+        </Typography>
+        <Grid>
+          <StatusCard
+            sm={1}
+            md={3}
+            title={i18n.t('processes.details.published')}
+            rightText=""
+          >
+            <h3>{processKeys?.encryptionPubKeys?.length ?? 0}</h3>
+          </StatusCard>
+          <StatusCard
+            md={3}
+            title={i18n.t('processes.details.revealed')}
+            rightText=""
+          >
+            <h3>{processKeys?.encryptionPrivKeys?.length ?? 0}</h3>
+          </StatusCard>
+        </Grid>
+      </>
+      }
     </>
   )
 }
