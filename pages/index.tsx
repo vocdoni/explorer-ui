fbcfe140553f32d2a8ece312ef0e6fac2a8762f8
selankon <selankon@selankon.xyz>
2022-08-02 10:29:59 +0200

    Fix loader is shown after first load

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ import {
 } from '@components/pages/home/components/hero-banner'
 import StatsPage from '@components/pages/stats'
 import { useStats } from '@hooks/use-stats'
+import i18n from '@i18n'
 import { ViewContext, ViewStrategy } from '@lib/strategy'
+import { Else, If, Then } from 'react-if'
 import FeaturedContent from './index/featured'
 
 // MAIN COMPONENT
@@ -13,31 +15,31 @@ const IndexPage = () => {
   const strategies: ViewStrategy[] = []
   const { loading: loadingStats, stats } = useStats({})
 
-  // todo(ritmo): implement error page?
-  const renderStatsPage = new ViewStrategy(
-    () => stats !== undefined && !loadingStats,
-    (
-      <>
-        <BannerContainer>
-          <HeroBanner
-            processes={stats?.process_count}
-            organizations={stats?.entity_count}
-            averageBlockTime={(stats?.block_time[0] || 0) / 1000}
-            envelopes={stats?.envelope_count}
-          />
-        </BannerContainer>
-        <StatsPage stats={stats} />
-        <FeaturedContent />
-      </>
-    )
+  return (
+    <If condition={loadingStats && stats === undefined}>
+      <Then>
+        <Loader visible />
+      </Then>
+      <Else>
+        {stats !== undefined ? (
+          <>
+            <BannerContainer>
+              <HeroBanner
+                processes={stats?.process_count}
+                organizations={stats?.entity_count}
+                averageBlockTime={(stats?.block_time[0] || 0) / 1000}
+                envelopes={stats?.envelope_count}
+              />
+            </BannerContainer>
+            <StatsPage stats={stats} />
+            <FeaturedContent />
+          </>
+        ) : (
+          <h1>{i18n.t('stats.stats_not_found')}</h1>
+        )}
+      </Else>
+    </If>
   )
-  strategies.push(renderStatsPage)
-  const renderLoadingPage = new ViewStrategy(() => true, <Loader visible />)
-  strategies.push(renderLoadingPage)
-
-  const viewContext = new ViewContext(strategies)
-
-  return viewContext.getView()
 }
 
 export default IndexPage
