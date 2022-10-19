82c7160d8d2e1ab3501290f37d473456efdc1456
selankon <selankon@selankon.xyz>
2022-10-19 19:58:47 +0200

    Implement validator card

--- components/blocks/card/validator-card.tsx
+++ components/blocks/card/validator-card.tsx
@@ -1,41 +1,67 @@
-import { ColumnProps } from '@components/elements/grid'
+import {
+  CardItemTitle,
+  GenericCardWrapper,
+  GenericCardWrapperProps,
+} from '@components/elements/card-generic'
 import { Validator } from '@lib/types'
-import { ValidatorPowerBadge } from '../badges/validator-badge'
-import { GenericListItemWithBadge } from '../list-items'
+import { theme } from '@theme/global'
+import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
+import { CopyButton } from '../copy-button'
+
 
 export const ValidatorCard = ({
-  sm,
-  md,
-  lg,
-  xl,
   validatorData,
-}: ColumnProps & {
+  ...props
+}: GenericCardWrapperProps & {
   validatorData: Validator
 }) => {
-  return (
-    <GenericListItemWithBadge
-      sm={sm}
-      md={md}
-      lg={lg}
-      xl={xl}
-      topLeft={
-        <>
+  const { i18n } = useTranslation()
 
-        </>
-      }
-      badge={<ValidatorPowerBadge power={validatorData.power}/> }
-      // dateText={localizedDateDiff(new Date(blockData?.timestamp))}
-      link={
-        null
-        // blockData?.height && !moreDetails
-        //   ? RouterService.instance.get(BLOCKS_DETAILS, {
-        //       blockHeight: blockData?.height?.toString(),
-        //     })
-        //   : null
-      }
-      title={validatorData.address}
-      dateText={"PubKey: " + validatorData.pubKey}
-    >
-    </GenericListItemWithBadge>
+  const Footer = () => {
+    return (
+      <FooterWrapper>
+        <Subtitle>
+          <div className="title">
+            {i18n.t('components.validator.Pubkey')} {': '}
+          </div>
+          <CopyButton
+            color={theme.textAccent1}
+            text={validatorData.pubKey}
+            toCopy={validatorData.pubKey}
+          ></CopyButton>
+        </Subtitle>
+        <Subtitle>
+          <div className="title">
+            {i18n.t('components.validator.voting_power')} {': '}
+          </div>
+          <div>{validatorData.power}</div>
+        </Subtitle>
+      </FooterWrapper>
+    )
+  }
+
+  return (
+    <GenericCardWrapper {...props} footer={<Footer />}>
+      <CardItemTitle>{validatorData.address}</CardItemTitle>
+    </GenericCardWrapper>
   )
 }
+
+const Subtitle = styled.div`
+  color: ${(props) => props.theme.lightText};
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 8px;
+
+  & > .title {
+    color: ${(props) => props.theme.text};
+    font-weight: bold;
+  }
+`
+
+const FooterWrapper = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+`
