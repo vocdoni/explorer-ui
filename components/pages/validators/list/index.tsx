661cf8f51e14520e35dce7dd4bbfcf0cd22afbec
selankon <selankon@selankon.xyz>
2022-10-19 20:01:56 +0200

    Fix validators header

--- components/pages/validators/list/index.tsx
+++ components/pages/validators/list/index.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next'
-import { ListPage } from '@components/pages/app/page-templates/list-page'
+import { InlineTitleChildrenContainer, ListPage } from '@components/pages/app/page-templates/list-page'
 import { Validator } from '@lib/types'
 import { ValidatorCard } from '@components/blocks/card/validator-card'
 
@@ -12,12 +12,13 @@ export const DashboardShowValidators = ({
 
   return (
     <>
-      <ListPage
-        title={i18n.t('validators_list.validators_list_title')}
-        subtitle={i18n.t('validators_list.validators_count', {
-          validatorsCount: validators.length,
-        })}
-      />
+     <InlineTitleChildrenContainer title={
+         <ListPage
+         title={i18n.t('validators_list.validators')}
+         subtitle={i18n.t('validators_list.count') + ": " + validators.length}
+       />
+      }>
+      </InlineTitleChildrenContainer>
 
       {validators.map((validator, i) => (
         <ValidatorCard key={i} validatorData={validator} />
