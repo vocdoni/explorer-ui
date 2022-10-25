5a0b2e444842aeb5d4d38506293f9afff95ddcb5
selankon <selankon@selankon.xyz>
2022-10-25 10:53:54 +0200

    Move copy alert msg near icon

--- components/blocks/copy-button.tsx
+++ components/blocks/copy-button.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import copy from 'copy-to-clipboard'
 import { useTranslation } from 'react-i18next'
-
-import { useAlertMessage } from '@hooks/message-alert'
-
 import { IoCopy } from 'react-icons/io5'
 import { IconContext } from 'react-icons'
+import styled from 'styled-components'
 
 export const CopyButton = ({
   toCopy,
@@ -22,29 +20,36 @@ export const CopyButton = ({
 }) => {
   const { i18n } = useTranslation()
 
-  const { setAlertMessage } = useAlertMessage()
+  const { setCopiedMessage, message } = useCopiedMessage()
   const handleCopy = (e) => {
     e.cancelBubble = true;
     e.stopPropagation();
     e.preventDefault();
+    e.target.focus();
     copy(toCopy)
-    setAlertMessage(i18n.t('copy.the_link_has_been_copied_to_the_clipboard'))
+    setCopiedMessage(i18n.t('copy.the_link_has_been_copied_to_the_clipboard'))
   }
 
   return (
-    <div onClick={handleCopy}>
-      <IconContext.Provider
-        value={{
-          color: color ?? 'inherit',
-          size: size ?? '1em',
-          style: style ?? { paddingLeft: '10px', cursor: 'pointer' },
-        }}
-      >
-        <div>
-          {text} <IoCopy />
-        </div>
-      </IconContext.Provider>
-    </div>
+    <>
+      <div onClick={handleCopy}>
+        <IconContext.Provider
+          value={{
+            color: color ?? 'inherit',
+            size: size ?? '1em',
+            style: style ?? { paddingLeft: '10px', cursor: 'pointer' },
+          }}
+        >
+          <TextAndIcon>
+            {text}
+              <AlertWrapper>
+                <IoCopy />
+                <CopiedAlert message={message}/>
+              </AlertWrapper>
+          </TextAndIcon>
+        </IconContext.Provider>
+      </div>
+    </>
   )
 }
 
@@ -63,4 +68,66 @@ export const ReducedTextAndCopy= ({
         text.substring(text.length - 4, text.length)
   return <CopyButton toCopy={toCopy} text={entityTxt} />
 }
+
+
+function useCopiedMessage() {
+  const [message, setMessage] = useState('')
+  const [timeout, setTimeoutTracker] = useState(null)
+
+  const setCopiedMessage = (msg: string, seconds = 4) => {
+    if (timeout) clearTimeout(timeout)
+
+    setMessage(msg)
+    const newTo = setTimeout(() => {
+      clearAlert()
+      setTimeoutTracker(null)
+    }, 1000 * seconds)
+    setTimeoutTracker(newTo)
+  }
+  const clearAlert = () => setMessage('')
+
+  return { message, setCopiedMessage, clearAlert }
+}
+
+const CopiedAlert = ({ message } : { message: string }) => {
+  return (
+    <AlertContainer visible={!!message?.length}>
+      <TextContainer>{message || 'The link has been copiet to the clipboard'}</TextContainer>
+    </AlertContainer>
+  )
+}
+
+
+const TextAndIcon = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
+
+const AlertWrapper = styled.div`
+  position: relative;
+`
+
+const AlertContainer = styled.div<{ visible: boolean }>`
+  position: absolute;
+  top: -35px;
+  left: 25px;
+  z-index: 5010;
+  font-size: 12px;
+  background-color: ${({ theme }) => theme.lightBg};
+  border: 1px solid ${({ theme }) => theme.lightBorder};
+  align-items: center;
+  padding: 8px;
+  border-radius: 8px;
+  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
+  opacity: ${({ visible }) => (visible ? '1' : '0')};
+  box-shadow: 0px 6px 6px rgba(180, 193, 228, 0.35);
+  transition: opacity 0.1s ease-out;
+  width: max-content;
+`
+
+const TextContainer = styled.p`
+margin: 0;
+max-width: 100%;
+`
 
