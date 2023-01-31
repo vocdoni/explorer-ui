39b60fda73df07d5cf8f6ab241a6fc2409093db3
selankon <selankon@selankon.xyz>
2023-01-31 10:42:03 +0100

    Upgrade to Next 11

--- next.config.js
+++ next.config.js
@@ -4,13 +4,15 @@ module.exports = {
   // Generate /dashboard/ instead of /dashboard.html
   trailingSlash: true,
   env,
-  webpack: (config, { isServer }) => {
+  webpack: (config, ) => {
     // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
+    config.resolve.fallback = {
+      fs: false,
+      crypto: false,
+      stream: false,
+      path: false,
+      os: false,
+    };
     return config
   }
 }
