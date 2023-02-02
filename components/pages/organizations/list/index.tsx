0890eb326a4a5cc571f40a880d427b7d840f54fd
selankon <selankon@selankon.xyz>
2023-02-02 15:09:58 +0100

    Implement organization count

--- components/pages/organizations/list/index.tsx
+++ components/pages/organizations/list/index.tsx
@@ -1,22 +1,23 @@
-import { useEntityCount } from '@hooks/use-entities'
 import { useTranslation } from 'react-i18next'
 import { ListPage } from '@components/pages/app/page-templates/list-page'
 import { DashboardEntityList } from './entity-list'
+import { useOrganizationCount } from '@hooks/use-voconi-sdk'
 
 export const DashboardShowEntities = () => {
   const { i18n } = useTranslation()
-  const { entitiesCount } = useEntityCount()
+  const { data: entitiesCount } = useOrganizationCount()
+  const count = entitiesCount?.count ?? 0
 
   const page_size = 10
 
   return (
     <DashboardEntityList
-      totalCount={entitiesCount}
+      totalCount={count}
       pageSize={page_size}
       title={
         <ListPage
           title={i18n.t('organizations.list.organizations_list_title')}
-          subtitle={i18n.t('organizations.count') + entitiesCount}
+          subtitle={i18n.t('organizations.count') + count}
         />
       }
     />
--- hooks/use-entities.tsx
+++ hooks/use-entities.tsx
@@ -55,35 +55,3 @@ export const useEntityList = ({
   }
 
 }
-
-/** Get entity count */
-export const useEntityCount = () => {
-  const { setAlertMessage } = useAlertMessage()
-  const { poolPromise } = usePool()
-
-  const [entitiesCount, setEntitiesCount] = useState(0)
-
-  const getEntitiesCountReq = useCallback(() => {
-    poolPromise
-      .then((pool) => {
-        return pool.sendRequest({
-          method: 'getEntityCount',
-        })
-      })
-      .then((response) => {
-        if (!response['ok'])
-          throw new Error('Error retrieving getProcessCount')
-          setEntitiesCount(response['size'])
-      })
-      .catch((err) => {
-        console.error(err)
-        setAlertMessage(i18n.t('error.could_not_fetch_organizations_count'))
-      })
-  }, [poolPromise, setAlertMessage])
-
-  useEffect(() => getEntitiesCountReq(), [getEntitiesCountReq])
-
-  return {
-    entitiesCount,
-  }
-}
