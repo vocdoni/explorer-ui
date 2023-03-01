f1be03b1c84492b9a5ae9fd59226a6514519cb0d
selankon <selankon@selankon.xyz>
2023-03-01 10:57:11 +0100

    Add date picker

---
+++ components/elements/date-picker.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+
+interface IDateTimePickerProps {
+  id?: string
+  value?: Date;
+  minDate?: Date;
+  onChange: (newDate: Date) => void;
+  required?: boolean
+}
+
+const DateTimePicker = ({
+                          id = "date-time-id",
+                          value = new Date(),
+                          minDate,
+                          onChange,
+                          required = false
+                        } : IDateTimePickerProps) => {
+
+  const [date, setDate] = useState(value);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newDate = new Date(event.target.value);
+    setDate(newDate);
+    onChange(newDate);
+  };
+
+  useEffect(() => {
+    setDate(value);
+  }, [value])
+
+  return (
+    <input
+      id={id}
+      type="datetime-local"
+      value={date.toISOString().substring(0, 16)}
+      min={minDate?.toISOString().substring(0, 16)}
+      onChange={handleChange}
+      required={required}
+    />
+  );
+}
+
+export default DateTimePicker;
