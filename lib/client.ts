de9fe3c2e29baab0d937169bcba0701a9ab260f1
selankon <selankon@selankon.xyz>
2023-03-22 15:32:26 +0100

    Fix generic hook

--- lib/client.ts
+++ lib/client.ts
@@ -5,12 +5,11 @@ export class ExtendedSDKClient extends VocdoniSDKClient {
     super(opts);
   }
 
-  txInfo = ({ txHash }: { txHash: string }) => ChainAPI.txInfo(this.url, txHash);
-  organizationList = ({ page = 0 }: { page?: number }) => ChainAPI.organizationList(this.url, page);
+  txInfo = (txHash: string) => ChainAPI.txInfo(this.url, txHash);
+  organizationList = (page?: number) => ChainAPI.organizationList(this.url, page);
   organizationCount = () => ChainAPI.organizationCount(this.url);
   validatorsList = () => ChainAPI.validatorsList(this.url);
-  voteInfo = ({ voteId }: { voteId: string }) => VoteAPI.info(this.url, voteId);
-  electionVotesList = ({ electionId, page }: { electionId: string; page?: number }) =>
-    ElectionAPI.votesList(this.url, electionId, page);
+  voteInfo = (voteId: string) => VoteAPI.info(this.url, voteId);
+  electionVotesList = (electionId: string, page?: number) => ElectionAPI.votesList(this.url, electionId, page);
   electionVotesCount = ({ electionId }: { electionId: string }) => ElectionAPI.votesCount(this.url, electionId);
 }
