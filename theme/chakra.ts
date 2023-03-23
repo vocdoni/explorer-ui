43a789c634c3b5afeb1108b5593a9bcc52becec5
selankon <selankon@selankon.xyz>
2023-03-23 10:02:51 +0100

    Create chakra lang selector

--- theme/chakra.ts
+++ theme/chakra.ts
@@ -1,13 +1,6 @@
 import { extendBaseTheme } from '@chakra-ui/react';
 import chakraTheme from '@chakra-ui/theme';
 
-const { Checkbox, Progress } = chakraTheme.components;
-
-const chakraDefaultTheme = extendBaseTheme({
-  components: {
-    Checkbox,
-    Progress,
-  },
-});
+const chakraDefaultTheme = extendBaseTheme({ components: chakraTheme.components });
 
 export default chakraDefaultTheme;
