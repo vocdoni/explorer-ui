ac8cf0b43f3c29b4c6da703920552020ae457bbd
selankon <selankon@selankon.xyz>
2023-04-13 17:37:56 +0200

    Implement interval on generic hook

--- hooks/use-voconi-sdk.tsx
+++ hooks/use-voconi-sdk.tsx
@@ -1,10 +1,18 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useClientContext } from '@vocdoni/react-components';
 import { ExtendedSDKClient } from '@lib/client';
 
 type PromiseReturnType<T> = T extends Promise<infer U> ? U : never;
 
-function useSDKFunction<T, U>(promiseFn: (params?: U) => Promise<T>, ...args: any[]) {
+function useSDKFunction<T, U>({
+  promiseFn,
+  args,
+  interval,
+}: {
+  promiseFn: (params?: U) => Promise<T>;
+  args?: any[];
+  interval?: number;
+}) {
   const [data, setData] = useState<PromiseReturnType<ReturnType<typeof promiseFn>> | null>(null);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState(false);
@@ -13,10 +21,7 @@ function useSDKFunction<T, U>(promiseFn: (params?: U) => Promise<T>, ...args: an
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const memorizedArgs = useMemo(() => args, args);
 
-  useEffect(() => {
-    setLoading(true);
-    setError(null);
-    setData(null);
+  const doCall = useCallback(() => {
     promiseFn(...memorizedArgs)
       .then((response) => {
         setData(response);
@@ -27,39 +32,51 @@ function useSDKFunction<T, U>(promiseFn: (params?: U) => Promise<T>, ...args: an
       .finally(() => {
         setLoading(false);
       });
-  }, [promiseFn, memorizedArgs]);
+  }, [memorizedArgs, promiseFn]);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    setData(null);
+    if (interval) {
+      const itv = setInterval(() => doCall(), interval);
+      doCall();
+      return () => clearInterval(itv);
+    }
+    doCall();
+  }, [promiseFn, memorizedArgs, doCall, interval]);
 
   return { data, error, loading };
 }
 
 export const useTxByHash = ({ txHash }: { txHash: string }) => {
   const { client } = useClientContext<ExtendedSDKClient>();
-  return useSDKFunction(client.txInfo, txHash);
+  return useSDKFunction({ promiseFn: client.txInfo, args: [txHash] });
 };
 
 // export const useOrganizationList = ({ page }: { page: number }) => useSDKFunction(ChainAPI.organizationList, page);
 
 export const useOrganizationCount = () => {
   const { client } = useClientContext<ExtendedSDKClient>();
-  return useSDKFunction(client.organizationCount);
+  return useSDKFunction({ promiseFn: client.organizationCount });
 };
 
 export const useValidators = () => {
   const { client } = useClientContext<ExtendedSDKClient>();
-  return useSDKFunction(client.validatorsList);
+  return useSDKFunction({ promiseFn: client.validatorsList });
 };
 
 export const useVoteInfo = ({ voteId }: { voteId: string }) => {
   const { client } = useClientContext<ExtendedSDKClient>();
-  return useSDKFunction(client.voteInfo, voteId);
+  return useSDKFunction({ promiseFn: client.voteInfo, args: [voteId] });
 };
 
 export const useElectionVotesList = ({ electionId, page }: { electionId: string; page?: number }) => {
   const { client } = useClientContext<ExtendedSDKClient>();
-  return useSDKFunction(client.electionVotesList, electionId, page);
+  return useSDKFunction({ promiseFn: client.electionVotesList, args: [electionId, page] });
 };
 
 export const useElectionVotesCount = ({ electionId }: { electionId: string }) => {
   const { client } = useClientContext<ExtendedSDKClient>();
-  return useSDKFunction(client.electionVotesCount, electionId);
+  return useSDKFunction({ promiseFn: client.electionVotesCount, args: [electionId] });
 };
