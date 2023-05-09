0a309efc2589a724eec72d84345fe0ca8d442add
selankon <selankon@selankon.xyz>
2023-05-09 11:08:58 +0200

    Implement block list by height

--- lib/client.ts
+++ lib/client.ts
@@ -1,4 +1,5 @@
 import { AccountAPI, ChainAPI, ClientOptions, ElectionAPI, VocdoniSDKClient, VoteAPI } from '@vocdoni/sdk';
+import { ChainAPI, ClientOptions, ElectionAPI, IChainBlockInfoResponse, VocdoniSDKClient, VoteAPI } from '@vocdoni/sdk';
 
 export class ExtendedSDKClient extends VocdoniSDKClient {
   constructor(opts: ClientOptions) {
@@ -16,7 +17,6 @@ export class ExtendedSDKClient extends VocdoniSDKClient {
   validatorsList = () => ChainAPI.validatorsList(this.url);
   chainInfo = () => ChainAPI.info(this.url);
   blockByHash = (hash: string) => ChainAPI.blockByHash(this.url, hash);
-  blockByHeight = (height: number) => ChainAPI.blockByHeight(this.url, height);
   voteInfo = (voteId: string) => VoteAPI.info(this.url, voteId);
   electionVotesList = (electionId: string, page?: number) => ElectionAPI.votesList(this.url, electionId, page);
   electionVotesCount = (electionId: string) => ElectionAPI.votesCount(this.url, electionId);
@@ -27,4 +27,16 @@ export class ExtendedSDKClient extends VocdoniSDKClient {
       status,
       withResults,
     });
+  blockByHeight = (height: number) => ChainAPI.blockByHeight(this.url, height);
+  blockList = (from: number, listSize: number | null = 10): Promise<IChainBlockInfoResponse[]> => {
+    const promises: Promise<IChainBlockInfoResponse>[] = [];
+    for (let i = 0; i < listSize; i++) {
+      // todo: this method will be fixed backend side, see https://github.com/vocdoni/interoperability/issues/33
+      promises.push(this.blockByHeight(from + i));
+    }
+    return Promise.all(promises).then((blockInfo) => {
+      // flatten the array[][] into array[]
+      return blockInfo.reduce((prev, cur) => prev.concat(cur), []);
+    });
+  };
 }
