70ac2657c35c4e49fb646313f9161f4b1474fec2
selankon <selankon@selankon.xyz>
2023-05-19 09:55:56 +0200

    Use SDK organizations list

--- components/blocks/card/process-card.tsx
+++ components/blocks/card/process-card.tsx
@@ -1,72 +1,73 @@
 import React from 'react';
 import styled from 'styled-components';
-import { SummaryProcess, useBlockHeight, useEntity } from '@vocdoni/react-hooks';
-import { EntityMetadata, ProcessSummary } from 'dvote-js';
 import { AnonVoteBadge, ProcessStatusBadge } from '../badges/process-status-badge';
 import { BodyWrapper, CardItemTitle, GenericCardWrapper, GenericCardWrapperProps } from '../../elements/card-generic';
 import { ReducedOrganizationNameWithIcon } from './entity-card';
 import { ensure0x } from '@vocdoni/common';
 import { ProcessTimeLeft } from '@components/blocks/process_time_left';
-import { getVoteStatus } from '@lib/util';
+import { ElectionProvider, useElection, OrganizationProvider } from '@vocdoni/chakra-components';
+import {
+  AllElectionStatus,
+  ElectionStatus,
+  ElectionStatusReady,
+  IElectionSummary,
+} from '../../../../VocdoniStack/vocdoni-sdk';
+import { PROCESS_DETAILS } from '@const/routes';
+import { getPath } from '@components/pages/app/components/get-links';
 
 type ProcessCardProps = GenericCardWrapperProps & {
-  process: SummaryProcess;
-  entityId?: string;
-  link?: string;
+  electionId: string;
+  electionSummary?: IElectionSummary;
   hideEntity?: boolean;
 };
 
-export const ProcessCard = ({ process, entityId, link }: ProcessCardProps) => {
-  const { metadata } = useEntity(ensure0x(entityId));
-  const entityMetadata = metadata as EntityMetadata;
-  const entityLogo = metadata?.media.avatar;
+export const ElectionCard = ({ electionId, ...rest }: ProcessCardProps) => {
+  return (
+    <ElectionProvider id={electionId}>
+      <InnerCard electionId={electionId} {...rest} />
+    </ElectionProvider>
+  );
+};
 
-  const t = process?.metadata?.title?.default;
-  const title = t && t.length > 0 ? t : process?.id;
-  const entityName = entityMetadata?.name?.default ? entityMetadata?.name?.default : entityId;
+const InnerCard = ({ electionId, electionSummary, hideEntity, ...rest }: ProcessCardProps) => {
+  const { election } = useElection();
+  const link = getPath(PROCESS_DETAILS, {
+    electionId: electionId,
+  });
+
+  const anonymous = election?.electionType?.anonymous ?? false;
+  const startDate = election?.startDate ?? new Date(electionSummary.startDate);
+  const endDate = election?.endDate ?? new Date(electionSummary.endDate);
+  const title = election?.title?.default ?? electionSummary.electionId;
+  const organizationId = election?.organizationId ?? electionSummary.organizationId;
+  const status = election?.status ?? getVoteStatus(electionSummary.status, startDate); // todo(kon): fix types here
 
   const Top = () => (
     <TopWrapper>
-      <StatusBadgeAndTimeLeft summary={process.summary} />
+      <ProcessStatusBadge status={status} />
+      {anonymous && <AnonVoteBadge />}
+      <ProcessTimeLeft status={status} endDate={endDate} startDate={startDate} />
     </TopWrapper>
   );
 
   const Footer = () => (
-    <OrganizationWrapper>
-      <ReducedOrganizationNameWithIcon
-        entityName={entityName}
-        entityId={entityId}
-        icon={entityLogo}
-      ></ReducedOrganizationNameWithIcon>
-    </OrganizationWrapper>
+    <OrganizationProvider id={organizationId}>
+      <EntityWrapper>
+        <ReducedOrganizationNameWithIcon organizationId={organizationId}></ReducedOrganizationNameWithIcon>
+      </EntityWrapper>
+    </OrganizationProvider>
   );
 
   return (
-    <GenericCardWrapper link={link} top={<Top />} footer={<Footer />}>
+    <GenericCardWrapper link={link} top={<Top />} footer={<Footer />} {...rest}>
       <BodyWrapper>
         <CardItemTitle>{title}</CardItemTitle>
-        {/* <CardItemSubTitle>
-          <strong>{i18n.t('processes.list.votes_submitted')}: </strong>xx
-        </CardItemSubTitle> */}
       </BodyWrapper>
     </GenericCardWrapper>
   );
 };
 
-const StatusBadgeAndTimeLeft = ({ summary }: { summary?: ProcessSummary }) => {
-  const { blockHeight } = useBlockHeight();
-  // todo(kon): use the sdk to do this. Actually it gets the election summary using the old RPC, mixing types.
-  const status = getVoteStatus(summary, blockHeight);
-  return (
-    <>
-      <ProcessStatusBadge status={status} />
-      {summary?.envelopeType.anonymous && <AnonVoteBadge />}
-      <ProcessTimeLeft status={status} summary={summary} />
-    </>
-  );
-};
-
-const OrganizationWrapper = styled.div`
+const EntityWrapper = styled.div`
   color: ${(props) => props.theme.textAccent1};
 `;
 
@@ -78,3 +79,12 @@ const TopWrapper = styled.div`
   padding: 0px;
   gap: 16px;
 `;
+
+// todo(kon): use SDK to do this conversion
+export const getVoteStatus = (status: AllElectionStatus, startDate: Date): ElectionStatus => {
+  return status === ElectionStatusReady.READY
+    ? startDate <= new Date()
+      ? ElectionStatus.ONGOING
+      : ElectionStatus.UPCOMING
+    : status;
+};
