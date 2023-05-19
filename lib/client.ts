f512a96f97ef9f2da567eedc0d247098c620fe5d
selankon <selankon@selankon.xyz>
2023-05-19 12:27:23 +0200

    Fix an import

--- lib/client.ts
+++ lib/client.ts
@@ -1,5 +1,4 @@
 import { ChainAPI, ClientOptions, ElectionAPI, VocdoniSDKClient, VoteAPI } from '@vocdoni/sdk';
-import { IElectionListFilter } from '../../VocdoniStack/vocdoni-sdk/src';
 
 export class ExtendedSDKClient extends VocdoniSDKClient {
   constructor(opts: ClientOptions) {
