1864e1f8c0638ce673fd49862fc19b96d12bc824
selankon <selankon@selankon.xyz>
2023-05-19 12:05:05 +0200

    Delete unused

--- hooks/use-processes.tsx
+++ hooks/use-processes.tsx
@@ -3,68 +3,7 @@ import { usePool, useProcesses } from '@vocdoni/react-hooks';
 import { useAlertMessage } from './message-alert';
 import i18n from '../i18n';
 import { utils } from 'ethers';
-import { VochainProcessStatus, VotingApi } from 'dvote-js';
 import { getProcessList } from '@lib/api';
-
-export interface useProcessListProps {
-  entityId?: string; // Deprecated, use search terms instead
-  namespace?: number;
-  status?: VochainProcessStatus;
-  withResults?: boolean;
-  from?: number;
-  searchTerm?: string;
-  listSize?: number;
-  reverse?: boolean;
-}
-
-export const useProcessesList = ({
-  entityId,
-  namespace,
-  status,
-  withResults,
-  from,
-  searchTerm,
-  listSize,
-  reverse = false,
-}: useProcessListProps) => {
-  const [processIds, setProcessIds] = useState<string[]>();
-  const [loadingProcessList, setLoadingProcessList] = useState(true);
-  const { setAlertMessage } = useAlertMessage();
-  const { poolPromise } = usePool();
-
-  // Loaders
-  const updateProcessIds = () => {
-    const f = from < 0 ? 0 : from;
-
-    setLoadingProcessList(true);
-
-    poolPromise
-      .then((pool) =>
-        VotingApi.getProcessList(
-          { entityId, namespace, status, withResults, from: f, searchTerm, listSize } as any,
-          pool
-        )
-      )
-      .then((ids) => {
-        setProcessIds(reverse ? ids.reverse() : ids);
-        setLoadingProcessList(false);
-      })
-      .catch((err) => {
-        setLoadingProcessList(false);
-        console.error(err);
-        setAlertMessage(i18n.t('errors.the_list_of_votes_cannot_be_loaded'));
-      });
-  };
-
-  useEffect(() => {
-    if (from || from === 0) updateProcessIds();
-  }, [entityId, namespace, status, withResults, from, searchTerm]);
-
-  return {
-    processIds,
-    loadingProcessList,
-  };
-};
 
 interface IgetProcessCountProps {
   entityId?: string;
