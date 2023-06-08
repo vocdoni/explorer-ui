5eec1454db2a69d24c5f0cab350e6653f43c4b40
selankon <selankon@selankon.xyz>
2023-06-08 09:06:15 +0200

    Minor fixes

--- components/pages/app/page-templates/list-page-filtered.tsx
+++ components/pages/app/page-templates/list-page-filtered.tsx
@@ -1,7 +1,7 @@
 import { PaginatorRouterParams } from '@components/blocks/paginator-router-params';
 import { FlexContainer, FlexAlignItem, FlexJustifyContent } from '@components/elements/flex';
 import { Column, ListCardContainer } from '@components/elements/grid';
-import { ReactNode, useCallback, useEffect, useState } from 'react';
+import { ReactNode, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Else, If, Then } from 'react-if';
 import { renderSkeleton } from './list-page';
@@ -13,10 +13,8 @@ interface IPaginatedListTemplateProps<Elements> {
   elementsList: Elements[];
   totalElementsCount: number;
   pageSize?: number;
-
   // Function that render map of elements
   renderElementFunction: (element: never, i: number) => ReactNode;
-
   currentPage: number;
   setCurrentPage: (x: number) => void;
 }
@@ -68,78 +66,6 @@ export const FilteredPaginatedList = <Elements,>({
   );
 };
 
-interface IUsePaginatedListProps<Filter> {
-  pageSize?: number;
-  // Paginate elements
-  setDataPagination: (newIndex: number) => void;
-  filter: Filter;
-  setFilter: (Filter: Filter) => void;
-  lastElement: number;
-}
-
-export function useFilteredPaginatedList<Filter>({
-  pageSize,
-  filter,
-  setFilter,
-  setDataPagination,
-  lastElement,
-}: IUsePaginatedListProps<Filter>) {
-  ///////////////////////////////
-  // PAGINATOR
-  ///////////////////////////////
-
-  // Paginator current page
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const getFirstPageIndex = useCallback((page) => lastElement - page * pageSize, [lastElement, pageSize]);
-
-  // When current page changed get next blocks
-  useEffect(() => {
-    if (lastElement) {
-      setDataPagination(getFirstPageIndex(currentPage));
-    }
-  }, [currentPage, getFirstPageIndex, lastElement, setDataPagination]);
-
-  ///////////////////////////////
-  // FILTER
-  ///////////////////////////////
-
-  // Return true if two JSON.stringify objects are equal
-  const compareJSONObjects = (obj1, obj2) => JSON.stringify(obj1) === JSON.stringify(obj2);
-
-  // Set the page at initial state
-  const resetPage = useCallback(() => {
-    setCurrentPage(1);
-    setDataPagination(0);
-  }, [setDataPagination]);
-
-  const enableFilter = (tempFilter) => {
-    if (!compareJSONObjects(filter, tempFilter)) {
-      resetPage();
-      setFilter({ ...tempFilter });
-    }
-  };
-
-  const disableFilter = (tempFilter, resetForm: { (): void }) => {
-    resetForm();
-    if (
-      Object.keys(filter).length !== 0 // Check if filter is already reset
-    ) {
-      resetPage();
-      setFilter({} as Filter);
-    }
-  };
-
-  return {
-    currentPage,
-    methods: {
-      enableFilter,
-      disableFilter,
-      setCurrentPage,
-    },
-  };
-}
-
 interface IUsePaginatedSDKListProps<Filter> {
   setCurrentPage: (newIndex: number) => void;
   filter: Filter;
@@ -168,9 +94,8 @@ export function useFilteredSDKPaginatedList<Filter>({
 
   const disableFilter = (tempFilter, resetForm: { (): void }) => {
     resetForm();
-    if (
-      Object.keys(filter).length !== 0 // Check if filter is already reset
-    ) {
+    // Check if filter is already reset
+    if (Object.keys(filter).length !== 0) {
       resetPage();
       setFilter({} as Filter);
     }
