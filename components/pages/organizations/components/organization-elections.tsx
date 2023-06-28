f3011a19ddd236162343b96a0ac7ecbe8c237f11
selankon <selankon@selankon.xyz>
2023-06-28 11:29:09 +0200

    Add paginator to organization elections

--- components/pages/organizations/components/organization-elections.tsx
+++ components/pages/organizations/components/organization-elections.tsx
@@ -1,32 +1,54 @@
 import { Column, Grid } from '@components/elements/grid';
 import { Typography, TypographyVariant } from '@components/elements/typography';
 import { ElectionCard } from '@components/blocks/card/process-card';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useOrganizationElectionsList } from '@hooks/use-voconi-sdk';
+import { useBlockList, useOrganizationElectionsList } from '@hooks/use-voconi-sdk';
+import { IChainBlockInfoResponse, IElectionSummary } from '@vocdoni/sdk';
+import { IFilterBlocks } from '@components/pages/blocks/components/block-filter';
+import { JumpToPaginatedList, useJumpToPaginatedList } from '@components/pages/app/page-templates/list-page-jump-to';
 
-export const OrganizationElections = ({ organizationId }: { organizationId: string }) => {
-  const { i18n } = useTranslation();
-  const { data: electionsList } = useOrganizationElectionsList({ organizationId: organizationId, page: 0 });
+export const OrganizationElections = ({
+  organizationId,
+  electionCount,
+}: {
+  organizationId: string;
+  electionCount: number;
+}) => {
+  const renderElectionCard = (election: IElectionSummary, i: number) => {
+    return <ElectionCard key={i} electionId={election.electionId} electionSummary={election} hideEntity={true} />;
+  };
+
+  const [paginatorPage, setPaginatorPage] = useState<number>(1);
+  const dataPage = paginatorPage - 1;
+
+  const { data: electionsList, loading } = useOrganizationElectionsList({
+    organizationId: organizationId,
+    page: dataPage,
+  });
   const elections = electionsList?.elections ?? [];
 
   return (
     <Grid>
       <Column sm={12}>
-        <Typography variant={TypographyVariant.Body1}>
-          {i18n.t('organizations.details.organization_processes')}{' '}
-        </Typography>
-        {elections.map((election, index) => {
-          return (
-            <ElectionCard key={index} electionId={election.electionId} electionSummary={election} hideEntity={true} />
-          );
-        })}
-        {!electionsList ||
-          (elections.length <= 0 && (
-            <Typography variant={TypographyVariant.Small}>
-              {i18n.t('organizations.details.no_processes_yet')}{' '}
-            </Typography>
-          ))}
+        <JumpToPaginatedList
+          loading={loading}
+          elementsList={elections}
+          totalElementsCount={electionCount}
+          renderElementFunction={renderElectionCard}
+          currentPage={paginatorPage}
+          setCurrentPage={setPaginatorPage}
+        />
+        {/*<Typography variant={TypographyVariant.Body1}>*/}
+        {/*  {i18n.t('organizations.details.organization_processes')}{' '}*/}
+        {/*</Typography>*/}
+        {/*{elections.map((election, index) => {})}*/}
+        {/*{!electionsList ||*/}
+        {/*  (elections.length <= 0 && (*/}
+        {/*    <Typography variant={TypographyVariant.Small}>*/}
+        {/*      {i18n.t('organizations.details.no_processes_yet')}{' '}*/}
+        {/*    </Typography>*/}
+        {/*  ))}*/}
       </Column>
     </Grid>
   );
