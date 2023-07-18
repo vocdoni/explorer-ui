966341bce47af52b982c43cfe87457972cdb8e2a
selankon <selankon@selankon.xyz>
2023-07-18 10:30:56 +0200

    Import TagProps

--- components/elements/CustomTag.tsx
+++ components/elements/CustomTag.tsx
@@ -1,7 +1,5 @@
-import { Tag } from '@chakra-ui/tag';
+import { Tag, TagProps } from '@chakra-ui/tag';
 import React, { PropsWithChildren } from 'react';
-
-type TagProps = React.ComponentProps<typeof Tag>;
 
 export const CustomTag = ({ children, ...rest }: PropsWithChildren<TagProps>) => (
   <Tag variant={'vocdoni'} {...rest}>
