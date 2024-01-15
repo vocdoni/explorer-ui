8485488e8d23027fcf373c6eab89885aec327bd9
selankon <selankon@selankon.xyz>
2024-01-15 11:12:02 +0100

    Fix #160
    Add InvalidElection badge

--- components/pages/elections/components/ElectionCard.tsx
+++ components/pages/elections/components/ElectionCard.tsx
@@ -1,11 +1,15 @@
 import { ProcessTimeLeft } from '@components/blocks/process_time_left';
 import { getPath } from '@components/pages/app/components/get-links';
 import { CustomElectionStatusBadge } from '@components/pages/elections/components/ElectionStatusBadge';
-import { AnonVoteBadge, ArchivedBadge } from '@components/pages/elections/components/ElectionTypeBadge';
+import {
+  AnonVoteBadge,
+  ArchivedBadge,
+  InvalidElectionBadge,
+} from '@components/pages/elections/components/ElectionTypeBadge';
 import { PROCESS_DETAILS } from '@const/routes';
 import useExtendedElection from '@hooks/use-extended-election';
 import { ElectionProvider, OrganizationProvider } from '@vocdoni/react-providers';
-import { IElectionSummary, PublishedElection } from '@vocdoni/sdk';
+import { IElectionSummary, InvalidElection, PublishedElection } from '@vocdoni/sdk';
 import styled from 'styled-components';
 import {
   BodyWrapper,
@@ -40,13 +44,17 @@ const InnerCard = ({ electionId, electionSummary, hideEntity, ...rest }: Process
   const title = election?.title?.default ?? electionId;
   const organizationId = election?.organizationId ?? electionSummary?.organizationId ?? null;
   const status = election?.status ?? PublishedElection.getStatus(electionSummary?.status, startDate) ?? null;
+  const isInvalid = election instanceof InvalidElection;
 
   const loading = electionLoading && !electionSummary;
 
   const Top = () => (
     <TopWrapper>
+      {isInvalid && <InvalidElectionBadge />}
       {election?.fromArchive ? <ArchivedBadge /> : <CustomElectionStatusBadge status={status} />}
-      {!election?.fromArchive && <ProcessTimeLeft status={status} endDate={endDate} startDate={startDate} />}
+      {!isInvalid && !election?.fromArchive && (
+        <ProcessTimeLeft status={status} endDate={endDate} startDate={startDate} />
+      )}
       {anonymous && <AnonVoteBadge />}
     </TopWrapper>
   );
